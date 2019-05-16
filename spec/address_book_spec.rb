2606f2841361bb2fee9eec0381c5d7e99af286ef
Samantha Acampora <samantha.acampora@gmail.com>
2019-05-15 20:47:05 -0400

    models assignment complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,21 @@ RSpec.describe AddressBook do
 			expect(new_entry.email).to eq('augusta.king@lovelace.com')
 		end
 	end
+
+	describe "#remove_entry" do 
+		it "removes an entry based on input name, phone_number, and email address" do
+		book = AddressBook.new
+		book.add_entry("Michael Faraday", "010.210.5181", "michael.faraday@faraday.com")
+
+		name = "Ada Lovelace"
+		phone_number = "010.012.1815"
+		email_address = "augusta.king@lovelace.com"
+		book.add_entry(name, phone_number, email_address)
+
+		expect(book.entries.size).to eq(2)
+		book.remove_entry(name, phone_number, email_address)
+		expect(book.entries.size).to eq(1)
+		expect(book.entries.first.name).to eq("Michael Faraday") 
+		end
+	end
 end
