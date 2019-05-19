57806c82c6adaa696344394de96087728b3493ae
Samantha Acampora <samantha.acampora@gmail.com>
2019-05-19 13:54:56 -0400

    searching assignment complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -47,4 +47,15 @@ require "csv"
 
 		return nil
 	end
+
+	def iterative_search(name)
+    	entries.each do |entry|
+    		if entry.name == name 
+    			return entry 
+    		end
+    	end
+
+    	return nil
+	end
+
 end
